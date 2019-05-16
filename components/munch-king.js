fd707eb32e3fd980d7d636be7b5223e3abf1ff6d
JoeyDeol <gurjothdeol@gmail.com>
2019-05-16 17:04:30 -0400

    Create base styles for munch king face

--- components/munch-king.js
+++ components/munch-king.js
@@ -1,14 +1,6 @@
 const munchKingTemplate = document.createElement('template');
 munchKingTemplate.innerHTML = `
   <style>
-    div {
-      border-radius: 90px;
-      background-color: pink;
-      width: 100px;
-      height: 100px;
-      transition: all 600ms cubic-bezier(0.77, 0, 0.175, 1);
-    }
-
     .chomping {
       background-color: blue;
     }
@@ -28,8 +20,55 @@ munchKingTemplate.innerHTML = `
     #bananas {
       background-color: yellow;
     }
+
+    .munch-king {
+      position: relative;
+      /* border-radius: 90px; */
+      border-top-left-radius: 80px 200px;
+      border-top-right-radius: 80px 200px;
+      border-bottom-left-radius: 100px 40px;
+      border-bottom-right-radius: 100px 40px;
+      background-color: rgb(24, 154, 211);
+      width: 300px;
+      height: 300px;
+      transition: all 600ms cubic-bezier(0.77, 0, 0.175, 1);
+    }
+    
+    .munch-king-eyes {
+      position: absolute;
+      top: 25%;
+      left: 50px;
+      display: flex;
+      justify-content: space-between;
+      width: 65%;
+      background-color: transparent;
+      border: 1px solid white;
+    }
+
+    .munch-king-eye {
+      width: 50px;
+      height: 50px;
+      background-color: firebrick;
+      border-radius: 90px;
+    }
+
+    .munch-king-mouth {
+      width: 50px;
+      height: 50px;
+      background-color: firebrick;
+      border-radius: 90px;
+    }
+
   </style>
-  <div class="munch-king"></div>
+  <div class="munch-king">
+    <div class="munch-king-eyes">
+      <div class="munch-king-eye left-eye"></div>
+      <div class="munch-king-eye right-eye"></div>
+    </div>
+    <div class="munch-king-mouth">
+      <slot name="mouth-style"><div class="munch-king-mouth-default"></div></slot>
+    </div>
+  </div>
 `;
 
 class MunchKing extends HTMLElement {
